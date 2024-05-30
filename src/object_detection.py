2339443d4f723c26adc6e45fc7d7ae9ec1ff2db7
Zaid Alsaheb <zaidsaheb4@gmail.com>
2024-05-30 17:08:29 +0000

    aruco detection stable

--- src/object_detection.py
+++ src/object_detection.py
@@ -1,14 +1,17 @@
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-import cv2
+import cv2 as cv
 import numpy as np
 
-class BlackObjectRecognition:
+class ArucoTagDetection:
     def __init__(self):
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/image_rect_color", Image, self.image_callback)
-        self.image_pub = rospy.Publisher("/image_with_rectangles", Image, queue_size=10)
+        self.image_pub = rospy.Publisher("/image_with_aruco", Image, queue_size=10)
+        self.dictionary = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_250)
+        self.parameters = cv.aruco.DetectorParameters()
+        self.detector = cv.aruco.ArucoDetector(self.dictionary, self.parameters)
 
     def image_callback(self, data):
         try:
@@ -17,27 +20,22 @@ class BlackObjectRecognition:
             rospy.logerr(e)
             return
 
-        # Convert image to grayscale
-        gray_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-        # Threshold the image to get only black regions
-        _, thresh = cv2.threshold(gray_image, 50, 255, cv2.THRESH_BINARY_INV)
-        # Find contours
-        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # Detect ArUco markers in the image
+        markerCorners, markerIds, rejectedCandidates = self.detector.detectMarkers(cv_image)
 
-        # Draw rectangles around the black objects
-        for contour in contours:
-            x, y, w, h = cv2.boundingRect(contour)
-            cv2.rectangle(cv_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        if markerIds is not None:
+            # Draw rectangles around the detected markers
+            cv.aruco.drawDetectedMarkers(cv_image, markerCorners, markerIds)
 
         try:
-            image_with_rectangles = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
-            self.image_pub.publish(image_with_rectangles)
+            image_with_aruco = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
+            self.image_pub.publish(image_with_aruco)
         except CvBridgeError as e:
             rospy.logerr(e)
 
 if __name__ == '__main__':
-    rospy.init_node('black_object_recognition', anonymous=True)
-    BlackObjectRecognition()
+    rospy.init_node('aruco_tag_detection', anonymous=True)
+    detector = ArucoTagDetection()
     try:
         rospy.spin()
     except KeyboardInterrupt:
