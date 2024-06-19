dc8efd82f68bd55f6bf69d224f4400d2a78291b5
Zaid Alsaheb <zaidsaheb4@gmail.com>
2024-06-19 14:04:58 +0000

    added threading to reduce lag

--- src/object_detection.py
+++ src/object_detection.py
@@ -3,7 +3,8 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import cv2 as cv
 import numpy as np
-from time import sleep
+from threading import Thread, Lock
+import queue
 
 class ArucoTagDetection:
     def __init__(self):
@@ -14,9 +15,15 @@ class ArucoTagDetection:
         self.parameters = cv.aruco.DetectorParameters()
         self.detector = cv.aruco.ArucoDetector(self.dictionary, self.parameters)
 
-        # Camera calibration parameters
-        self.camera_matrix = np.array([[836.527947, 0, 808.422471], [0, 839.354724, 588.0755], [0, 0, 1]])  # Replace fx, fy, cx, cy with your calibration values
-        self.dist_coeffs = np.array([-0.262186, 0.048066, 0.001499, -0.000339, 0.000000])  # Replace k1, k2, p1, p2, k3 with your distortion coefficients
+        self.camera_matrix = np.array([[836.527947, 0, 808.422471], [0, 839.354724, 588.0755], [0, 0, 1]])  # Replace with your calibration values
+        self.dist_coeffs = np.array([-0.262186, 0.048066, 0.001499, -0.000339, 0.000000])  # Replace with your distortion coefficients
+
+        self.image_queue = queue.Queue(maxsize=1)
+        self.lock = Lock()
+
+        self.processing_thread = Thread(target=self.process_images)
+        self.processing_thread.daemon = True
+        self.processing_thread.start()
 
     def image_callback(self, data):
         try:
@@ -25,28 +32,32 @@ class ArucoTagDetection:
             rospy.logerr(e)
             return
 
-        # Detect ArUco markers in the image
-        markerCorners, markerIds, rejectedCandidates = self.detector.detectMarkers(cv_image)
+        with self.lock:
+            if not self.image_queue.full():
+                self.image_queue.put(cv_image)
 
-        if markerIds is not None:
-            # Draw rectangles around the detected markers
-            cv.aruco.drawDetectedMarkers(cv_image, markerCorners, markerIds)
+    def process_images(self):
+        while not rospy.is_shutdown():
+            if not self.image_queue.empty():
+                with self.lock:
+                    cv_image = self.image_queue.get()
 
-            # Estimate pose of each marker
-            rvecs, tvecs, _ = cv.aruco.estimatePoseSingleMarkers(markerCorners, 0.14, self.camera_matrix, self.dist_coeffs)
+                markerCorners, markerIds, rejectedCandidates = self.detector.detectMarkers(cv_image)
 
-            # Draw axis for each marker
-            for i in range(len(markerIds)):
-                cv.drawFrameAxes(cv_image, self.camera_matrix, self.dist_coeffs, rvecs[i], tvecs[i], 0.05)
-        else:
-            rospy.loginfo("No markers detected")
-            sleep(6)
+                if markerIds is not None:
+                    cv.aruco.drawDetectedMarkers(cv_image, markerCorners, markerIds)
+                    rvecs, tvecs, _ = cv.aruco.estimatePoseSingleMarkers(markerCorners, 0.14, self.camera_matrix, self.dist_coeffs)
 
-        try:
-            image_with_aruco = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
-            self.image_pub.publish(image_with_aruco)
-        except CvBridgeError as e:
-            rospy.logerr(e)
+                    for i in range(len(markerIds)):
+                        cv.drawFrameAxes(cv_image, self.camera_matrix, self.dist_coeffs, rvecs[i], tvecs[i], 0.05)
+                else:
+                    rospy.loginfo("No markers detected")
+
+                try:
+                    image_with_aruco = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
+                    self.image_pub.publish(image_with_aruco)
+                except CvBridgeError as e:
+                    rospy.logerr(e)
 
 if __name__ == '__main__':
     rospy.init_node('aruco_tag_detection', anonymous=True)
